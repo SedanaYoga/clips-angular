d58de37cce2edb6d2b18d74a4ff35c4cf43a3e12
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-03 21:16:30 +0800

    undo the changes of memory leak example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core'
-import { ModalService } from './services/modal.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  showModal = true
-
-  constructor(public modal: ModalService) {}
-
-  ngOnInit() {
-    setInterval(() => {
-      this.showModal = !this.showModal
-      console.log(this.modal.modals)
-    }, 1000)
-  }
-}
+export class AppComponent {}
