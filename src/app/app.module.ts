f5e07074ff1f5ca8b53e530012d1eaebe2215aec
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-20 17:11:24 +0800

    Add notfound page with WildCard and rearrange routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,29 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { environment } from 'src/environments/environment'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
-import { VideoModule } from './video/video.module';
+import { VideoModule } from './video/video.module'
 import { ClipComponent } from './clip/clip.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent, ClipComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ClipComponent,
+    NotFoundComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     VideoModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
