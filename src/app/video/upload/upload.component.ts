43106f220341e8793eb089fe33ce06cfc2e820f5
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-23 11:52:51 +0800

    Subscribe to snapshot to get Success and Error alert

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AngularFireStorage } from '@angular/fire/compat/storage'
 import { v4 as uuid } from 'uuid'
+import { last } from 'rxjs'
 
 @Component({
   selector: 'app-upload',
@@ -17,6 +18,7 @@ export class UploadComponent implements OnInit {
   alertMsg = 'Please wait, your clip is being uploaded'
   inSubmission = false
   percentage = 0
+  showPercentage = false
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -48,6 +50,7 @@ export class UploadComponent implements OnInit {
     this.alertColor = 'blue'
     this.alertMsg = 'Please wait, your clip is being uploaded'
     this.inSubmission = true
+    this.showPercentage = true
 
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
@@ -56,5 +59,24 @@ export class UploadComponent implements OnInit {
     task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100
     })
+
+    task
+      .snapshotChanges()
+      .pipe(last())
+      .subscribe({
+        next: (snapshot) => {
+          this.alertColor = 'green'
+          this.alertMsg =
+            'Success! Your clip is now ready to share with the world'
+          this.showPercentage = false
+        },
+        error: (error) => {
+          this.alertColor = 'red'
+          this.alertMsg = 'Upload failed! Please try again later'
+          this.inSubmission = true
+          this.showPercentage = false
+          console.error(error)
+        },
+      })
   }
 }
