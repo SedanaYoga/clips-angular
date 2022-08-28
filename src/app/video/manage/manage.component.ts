d0a130222001243c4809f9e4c31d72f65cd37876
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-28 15:08:47 +0800

    Sorting using Behavior Subject and added Composite Indexes in Firebase console

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router'
 import IClip from 'src/app/models/clip.model'
 import { ClipService } from 'src/app/services/clip.service'
 import { ModalService } from 'src/app/services/modal.service'
+import { BehaviorSubject } from 'rxjs'
 
 @Component({
   selector: 'app-manage',
@@ -13,20 +14,24 @@ export class ManageComponent implements OnInit {
   videoOrder = '1'
   clips: IClip[] = []
   activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clipService: ClipService,
     private modal: ModalService,
-  ) {}
+  ) {
+    this.sort$ = new BehaviorSubject(this.videoOrder)
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params.sort === '2' ? params.sort : '1'
+      this.sort$.next(this.videoOrder)
     })
 
-    this.clipService.getUserClips().subscribe((docs) => {
+    this.clipService.getUserClips(this.sort$).subscribe((docs) => {
       this.clips = []
 
       docs.forEach((doc) => {
@@ -59,6 +64,18 @@ export class ManageComponent implements OnInit {
     this.clips.forEach((element, index) => {
       if (element.docID === $event.docID) {
         this.clips[index].title = $event.title
+      }
+    })
+  }
+
+  delete($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.clipService.deleteClip(clip)
+
+    this.clips.forEach((element, index) => {
+      if (element.docID === clip.docID) {
+        this.clips.splice(index, 1)
       }
     })
   }
