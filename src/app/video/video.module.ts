0dfbcb3ff29672dc851b261525435b4452c7de04
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-28 11:47:28 +0800

    Edit Clip modal and form

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -5,10 +5,11 @@ import { VideoRoutingModule } from './video-routing.module'
 import { ManageComponent } from './manage/manage.component'
 import { UploadComponent } from './upload/upload.component'
 import { SharedModule } from '../shared/shared.module'
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditComponent } from './edit/edit.component'
 
 @NgModule({
-  declarations: [ManageComponent, UploadComponent],
+  declarations: [ManageComponent, UploadComponent, EditComponent],
   imports: [
     CommonModule,
     VideoRoutingModule,
