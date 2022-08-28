d9cee411dc4e70677dbba26c13c064b4459f4072
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-28 10:43:07 +0800

    Storing timestamp in Clip

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -9,6 +9,7 @@ import firebase from 'firebase/compat/app'
 import { v4 as uuid } from 'uuid'
 import { last, switchMap } from 'rxjs'
 import { ClipService } from 'src/app/services/clip.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-upload',
@@ -41,6 +42,7 @@ export class UploadComponent implements OnDestroy {
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
     private clipsService: ClipService,
+    private router: Router,
   ) {
     auth.user.subscribe((user) => (this.user = user))
   }
@@ -88,22 +90,26 @@ export class UploadComponent implements OnDestroy {
         switchMap(() => clipRef.getDownloadURL()),
       )
       .subscribe({
-        next: (url) => {
+        next: async (url) => {
           const clip = {
             uid: this.user?.uid as string,
             displayName: this.user?.displayName as string,
             title: this.title.value,
             fileName: `${clipFileName}.mp4`,
             url,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
           }
 
-          this.clipsService.createClip(clip)
+          const clipDocRef = await this.clipsService.createClip(clip)
 
-          console.log(clip)
           this.alertColor = 'green'
           this.alertMsg =
             'Success! Your clip is now ready to share with the world'
           this.showPercentage = false
+
+          setTimeout(() => {
+            this.router.navigate(['clip', clipDocRef.id])
+          }, 1000)
         },
         error: (error) => {
           this.uploadForm.enable()
