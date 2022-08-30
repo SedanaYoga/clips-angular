a025656cc3a3ed2ffb7e8283def6e911879b716e
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-30 21:59:47 +0800

    Initializing FFmpeg

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -10,6 +10,7 @@ import { v4 as uuid } from 'uuid'
 import { last, switchMap } from 'rxjs'
 import { ClipService } from 'src/app/services/clip.service'
 import { Router } from '@angular/router'
+import { FfmpegService } from 'src/app/services/ffmpeg.service'
 
 @Component({
   selector: 'app-upload',
@@ -43,8 +44,10 @@ export class UploadComponent implements OnDestroy {
     private auth: AngularFireAuth,
     private clipsService: ClipService,
     private router: Router,
+    public ffmpegService: FfmpegService,
   ) {
     auth.user.subscribe((user) => (this.user = user))
+    this.ffmpegService.init()
   }
 
   ngOnDestroy(): void {
