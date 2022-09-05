20c885092976a1999bff968a286d1478a028f594
Sedana Yoga <9g.yoga27@gmail.com>
2022-09-05 20:25:37 +0800

    Generate Clip List component

---
+++ src/app/clip-list/clip-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clip-list',
+  templateUrl: './clip-list.component.html',
+  styleUrls: ['./clip-list.component.scss']
+})
+export class ClipListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
