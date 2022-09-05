51fa6c0226fb634dad856c368dd5f45de5d3be28
Sedana Yoga <9g.yoga27@gmail.com>
2022-09-05 20:36:23 +0800

    Handling scroll event in Clip List component

--- src/app/clip-list/clip-list.component.ts
+++ src/app/clip-list/clip-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { log } from 'console'
 
 @Component({
   selector: 'app-clip-list',
   templateUrl: './clip-list.component.html',
-  styleUrls: ['./clip-list.component.scss']
+  styleUrls: ['./clip-list.component.scss'],
 })
-export class ClipListComponent implements OnInit {
-
-  constructor() { }
+export class ClipListComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
   }
 
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement
+    const { innerHeight } = window
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if (bottomOfWindow) {
+      console.log('bottom of window')
+    }
+  }
 }
